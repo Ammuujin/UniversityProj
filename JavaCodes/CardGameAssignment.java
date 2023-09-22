16ec04c719afd5e65b4fa7bfbd7f2c3f0fd5c817
Amuujin Erdenesaikhan <amuujine@gmail.com>
2023-09-22 11:33:54 +0900

    editted some java codes

--- JavaCodes/CardGameAssignment.java
+++ JavaCodes/CardGameAssignment.java
@@ -1,139 +1,127 @@
-package JavaCodes;
+// package JavaCodes;
 
-// 파일명: CardGame.java import java.util.Scanner
-import java.util.Scanner;
-// Class 이름과 파일명을 일치시킴.
-// main()을 포함한 class는 class name 앞에 public을 반드시 붙인다.
-//We have dealer and players
-public class CardGameAssignment {
-    public static void main(String[] args) {
-        //Participators declaration
-        int playerNum = 0;
-        //arrays storing name's and money's of players 
-        String[] name = new String[10];
-        int[] money = new int[10];
-        //Card declaration
-        Card d = new Card(); // Card for dealer
-        int rank, suit;
-        int result = 0;
-        Scanner in = new Scanner(System.in);
-        // 게임을 반복적으로 실행함
-        do {
-            System.out.println("Game started...");
-            System.out.println("Enter the number of players: ");
-            playerNum = in.nextInt();
-            int i = 0; 
-            while (i < playerNum){
-                System.out.println("Enter the player"+ (i+1) + ("(name, game money): "));
-                name[i] = in.next();
-                money[i] = in.nextInt();
-                i++;
-            }
-            System.out.print("Input your card, suit(1-4) and rank(1-13)! : ");
-            suit = in.nextInt();
-            rank = in.nextInt();
-            if (suit < 1 || suit > 4) {
-                System.out.println("Enter suit and rank again!");
-                continue;
-            }
-
-            //We have 52 different cards. Now give a every player random card from 52 cards.
-            i=0;
-            while( i < playerNum ){
-                //Give card to players from 52 cards.
-            }
-            d.suit = suitInString(suit);
-            c1.suit = suitInString(suit);
-            c2.suit = suitInString(suit);
-            if (rank < 1 || rank > 13) {
-                System.out.println("Enter suit and rank again!");
-                continue;
-            }
-            c1.rank = rank;
-            suit = (int) (Math.random() * 4) + 1; // generate a random number (1~4)
-            rank = (int) (Math.random() * 13) + 1; // generate a rendom number (1~13)
-            c2.rank = rank;
-            System.out.println(toString(c1));
-            System.out.println(toString(c2));
-            result = compare(c1, c2);
-            printResult(result);
-        } while (result < 1);
-        in.close();
-    }
-    // print results
-    static void printResult(int result) {
-        if (result > 0) {
-            System.out.print("Winner: ");
-            System.out.println("Player");
-        } else if (result < 0) {
-            System.out.print("Winner: ");
-            System.out.println("Computer");
-        } else {
-            System.out.println("Tie");
-        }
-    }
-    //Card class
-    static class Card{
-        String suit;
-        int rank;
-    }
-    // suitInString() method
-    static String suitInString(int suit){
-        String s = "";
-        switch(suit){
-            case 1: s = "Spade"; break;
-            case 2: s = "Diamond"; break;
-            case 3: s = "Heart"; break;
-            case 4: s = "Club"; break;
-        }
-        return s;
-    }
-    // toString() method
-    static String toString(Card c)
-    {
-        String s = "";
-        switch(c.rank){
-            case 1: s = "Ace"; break;
-            case 11: s = "Jack"; break;
-            case 12: s = "Queen"; break;
-            case 13: s = "King"; break;
-            default: s = Integer.toString(c.rank); break;
-        }
-        return s + " of " + c.suit;
-    }
-    // compare() method 
-    //Spade > Diamond > Heart > Club
-    static int compare(Card c1, Card c2){
-        int result = 0;
-        if(c1.rank > c2.rank){
-            result = 1;
-        }
-        else if(c1.rank < c2.rank){
-            result = -1;
-        }
-        else if (c1.rank == c2.rank && c1.suit!=c2.suit){
-            if(c1.suit=="Spade" && (c2.suit=="Diamond" || c2.suit=="Heart" || c2.suit=="Club")){
-                result = 1;
-            }
-            else if(c2.suit=="Spade" && (c1.suit=="Diamond" || c1.suit=="Heart" || c1.suit=="Club")){
-                result = -1;
-            }
-            else if(c1.suit=="Diamond" && (c2.suit=="Heart" || c2.suit=="Club")){
-                result = 1;
-            }
-            else if(c2.suit=="Diamond" && (c1.suit=="Heart" || c1.suit=="Club")){
-                result = -1;
-            }
-            else if(c1.suit=="Heart" && c2.suit=="Club"){
-                result = 1;
-            }
-            else if(c2.suit=="Heart" && c1.suit=="Club"){
-                result = -1;
-            }
-        }
-        else if(c1.rank == c2.rank && c1.suit==c2.suit){
-            result = 0;
-        }
-        return result;
-    }
-} // end of class CardGame
+// //package Week3;
+// // 파일명: CardGame.java import java.util.Scanner;
+// import java.util.Scanner;
+// // Class 이름과 파일명을 일치시킴.
+// // main()을 포함한 class는 class name 앞에 public을 반드시 붙인다.
+// public class CardGameAssignment {
+//     static Card[] deck;// Card 20장을 저장하기 위한 배열
+//     static int count; // deck에 남은 카드의 수
+//     public static void main(String[] args) {
+//         deck = new Card[20];
+//         count = 20;
+//         Card dealer;
+//         Card player;
+//         int result;
+//         initialize();// Card 20장을 생성하여 CardStack에 저장하는 method
+//         // 게임을 반복적으로 실행함
+//         do {
+//             player = deal(); // Card의 배열에서 한장을 뽑는다.
+//             dealer = deal(); // Card의 배열에서 한장을 뽑는다.
+//             if(player==null || dealer==null){
+//                 System.out.println("No more cards...");
+//                 break;
+//             }
+//             System.out.println(toString(player));
+//             System.out.println(toString(dealer));
+//             result = compare(player, dealer);
+//             //until player wins the game will be continued
+//             if(result > 0){
+//                 System.out.println("Winner Player: "+toString(player));
+//             }
+//             else if(result < 0){
+//                 System.out.println("Winner Dealer: "+toString(dealer));
+//             }
+//             else{
+//                 System.out.println("Tie!");
+//             }
+//         } while (result < 1);
+//     }
+//     static void initialize(){
+//         int i = 0;
+//         for(int suit=1; suit<=4; suit++){
+//             for(int rank=1; rank<=13; rank++){
+//                 deck[i] = new Card();
+//                 deck[i].suit = suitInString(suit);
+//                 deck[i].rank = rank;
+//                 i++;
+//             }
+//         }
+//     }
+//     static Card deal(){
+//         Card c = null;
+//         if(count > 0){
+//             int index = (int)(Math.random()*count);
+//             c = deck[index];
+//             deck[index] = deck[count-1];
+//             deck[count-1] = null;
+//             count--;
+//         }
+//         return c;
+//     }
+//     //Card class
+//     static class Card{
+//         String suit;
+//         int rank;
+//     }
+//     // suitInString() method
+//     static String suitInString(int suit){
+//         String s = "";
+//         switch(suit){
+//             case 1: s = "Spade"; break;
+//             case 2: s = "Diamond"; break;
+//             case 3: s = "Heart"; break;
+//             case 4: s = "Club"; break;
+//         }
+//         return s;
+//     }
+//     // toString() method
+//     static String toString(Card c)
+//     {
+//         String s = "";
+//         switch(c.rank){
+//             case 1: s = "Ace"; break;
+//             case 11: s = "Jack"; break;
+//             case 12: s = "Queen"; break;
+//             case 13: s = "King"; break;
+//             default: s = Integer.toString(c.rank); break;
+//         }
+//         return s + " of " + c.suit;
+//     }
+//     // compare() method 
+//     //Spade > Diamond > Heart > Club
+//     static int compare(Card c1, Card c2){
+//         int result = 0;
+//         if(c1.rank > c2.rank){
+//             result = 1;
+//         }
+//         else if(c1.rank < c2.rank){
+//             result = -1;
+//         }
+//         else if (c1.rank == c2.rank && c1.suit!=c2.suit){
+//             if(c1.suit=="Spade" && (c2.suit=="Diamond" || c2.suit=="Heart" || c2.suit=="Club")){
+//                 result = 1;
+//             }
+//             else if(c2.suit=="Spade" && (c1.suit=="Diamond" || c1.suit=="Heart" || c1.suit=="Club")){
+//                 result = -1;
+//             }
+//             else if(c1.suit=="Diamond" && (c2.suit=="Heart" || c2.suit=="Club")){
+//                 result = 1;
+//             }
+//             else if(c2.suit=="Diamond" && (c1.suit=="Heart" || c1.suit=="Club")){
+//                 result = -1;
+//             }
+//             else if(c1.suit=="Heart" && c2.suit=="Club"){
+//                 result = 1;
+//             }
+//             else if(c2.suit=="Heart" && c1.suit=="Club"){
+//                 result = -1;
+//             }
+//         }
+//         else if(c1.rank == c2.rank && c1.suit==c2.suit){
+//             result = 0;
+//         }
+//         return result;
+//     }
+// } // end of class CardGame
