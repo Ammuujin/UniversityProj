550fb57842ca8f50c659f52eff09cfcaa7fd5906
Amuujin Erdenesaikhan <amuujine@gmail.com>
2023-09-28 21:10:31 +0900

    finalized converter in c

--- DataStructure/converter.c
+++ DataStructure/converter.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ctype.h> // Include ctype.h for isdigit function
+#include <ctype.h>
 
-// Function to convert a number from one base to another
 void convertBase(char *number, int base1, int base2) {
     int num = 0;
     int length = strlen(number);
     
     // Convert the number from base1 to base 10
     for (int i = 0; i < length; i++) {
-        int digit = (isdigit(number[i])) ? (number[i] - '0') : (number[i] - 'A' + 10);
+        int digit;
+        if (isdigit(number[i])) {
+            digit = number[i] - '0';
+        } else {
+            digit = number[i] - 'A' + 10;
+        }
         num = num * base1 + digit;
     }
     
@@ -19,7 +22,11 @@ void convertBase(char *number, int base1, int base2) {
     int index = 0;
     while (num > 0) {
         int digit = num % base2;
-        result[index] = (digit < 10) ? ('0' + digit) : ('A' + digit - 10);
+        if (digit < 10) {
+            result[index] = '0' + digit;
+        } else {
+            result[index] = 'A' + digit - 10;
+        }
         num /= base2;
         index++;
     }
@@ -31,26 +38,15 @@ void convertBase(char *number, int base1, int base2) {
     }
     printf("\n");
 }
-
 int main() {
-    int base1, base2;
     char number[100];
-
-    printf("Enter the base of your number: ");
-    scanf("%d", &base1);
-
+    int base1, base2;
     printf("Enter the number: ");
     scanf("%s", number);
-
-    printf("Enter the base you want to convert: ");
+    printf("Enter the base of the number: ");
+    scanf("%d", &base1);
+    printf("Enter the base to convert to: ");
     scanf("%d", &base2);
-
-    if (base1 < 2 || base1 > 36 || base2 < 2 || base2 > 36) {
-        printf("Invalid base. Please enter a base between 2 and 36.\n");
-        return 1;
-    }
-
     convertBase(number, base1, base2);
-
     return 0;
 }
