8acbe4b6c7098456f3751a4f5ec25f68d4e74870
Amuujin Erdenesaikhan <amuujine@gmail.com>
2023-10-10 20:09:01 +0900

    some features in converter code

--- DataStructure/converter.c
+++ DataStructure/converter.c
@@ -2,51 +2,55 @@
 #include <string.h>
 #include <ctype.h>
 
-void convertBase(char *number, int base1, int base2) {
-    int num = 0;
-    int length = strlen(number);
-    
-    // Convert the number from base1 to base 10
-    for (int i = 0; i < length; i++) {
-        int digit;
-        if (isdigit(number[i])) {
-            digit = number[i] - '0';
-        } else {
-            digit = number[i] - 'A' + 10;
-        }
-        num = num * base1 + digit;
-    }
-    
-    // Convert the number from base 10 to base2
-    char result[100];
-    int index = 0;
-    while (num > 0) {
-        int digit = num % base2;
-        if (digit < 10) {
-            result[index] = '0' + digit;
-        } else {
-            result[index] = 'A' + digit - 10;
-        }
-        num /= base2;
-        index++;
-    }
-    
-    // Print the result in reverse order
-    printf("The number in base %d is: ", base2);
-    for (int i = index - 1; i >= 0; i--) {
-        printf("%c", result[i]);
-    }
-    printf("\n");
-}
+// Prototype
+void convert(int num, int base, char *result);
+void reverseString(char *str);
+
+// Main function
 int main() {
-    char number[100];
-    int base1, base2;
-    printf("Enter the number: ");
-    scanf("%s", number);
-    printf("Enter the base of the number: ");
-    scanf("%d", &base1);
-    printf("Enter the base to convert to: ");
-    scanf("%d", &base2);
-    convertBase(number, base1, base2);
+    //d is the number in base 10, x is the base to convert to
+    int d, x;
+    char result[100];
+    printf("Enter a number in base 10: ");
+    scanf("%d", &d);
+    if(d < 0) {
+        printf("Invalid input\n");
+        return 0;
+    }else{
+        printf("Enter the base to convert to: ");
+        scanf("%d", &x);
+        if(x>0){
+            convert(d, x, result);
+            printf("The number %d in base %d is %s\n", d, x, result);
+        }else{
+            printf("Invalid input\n");
+        }
+    }
     return 0;
+}
+
+// Function to convert a number from base 10 to a given base
+void convert(int num, int base, char *result) {
+    int i = 0;
+    while (num > 0) {
+        int rem = num % base;
+        if (rem < 10)
+            result[i++] = rem + '0';
+        else
+            result[i++] = rem - 10 + 'A';
+        num /= base;
+    }
+    result[i] = '\0';
+    reverseString(result); // Reverse the result string
+}
+
+// Function to reverse a string
+void reverseString(char *str) {
+    int length = strlen(str);
+    int i, j;
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
 }
