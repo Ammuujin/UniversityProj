84a90a1af709ec144b3bd37795618afc368a9005
Amuujin Erdenesaikhan <amuujine@gmail.com>
2023-10-10 20:44:51 +0900

    finalize the converter code in c

--- DataStructure/converter.c
+++ DataStructure/converter.c
@@ -1,56 +1,71 @@
+//Program that converts a decimal number to a given base number and vice versa.
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
-// Prototype
-void convert(int num, int base, char *result);
-void reverseString(char *str);
+// Function prototypes
+int task1(int number, int base);
+int task2(char *number, int base);
 
 // Main function
 int main() {
-    //d is the number in base 10, x is the base to convert to
-    int d, x;
-    char result[100];
-    printf("Enter a number in base 10: ");
-    scanf("%d", &d);
-    if(d < 0) {
-        printf("Invalid input\n");
-        return 0;
-    }else{
-        printf("Enter the base to convert to: ");
-        scanf("%d", &x);
-        if(x>0){
-            convert(d, x, result);
-            printf("The number %d in base %d is %s\n", d, x, result);
-        }else{
-            printf("Invalid input\n");
-        }
+    int task, base;
+    char number[100];
+    printf("Enter your inputs: \n ");
+    scanf("%d", &task);
+    scanf("%s %d", number, &base);
+    if (task == 1) {
+        task1(atoi(number), base);
+    } else if (task == 2) {
+        task2(number, base);
+    } else {
+        printf("Invalid task number\n");
     }
     return 0;
 }
 
-// Function to convert a number from base 10 to a given base
-void convert(int num, int base, char *result) {
+// Task 1: Converting decimal number to a given base number.
+int task1(int number, int base) {
     int i = 0;
-    while (num > 0) {
-        int rem = num % base;
-        if (rem < 10)
-            result[i++] = rem + '0';
-        else
-            result[i++] = rem - 10 + 'A';
-        num /= base;
+    char result[100]; 
+    while (number != 0) {
+        int remainder = number % base;
+        if (remainder < 10) {
+            result[i] = remainder + '0';  
+        } else {
+            result[i] = remainder - 10 + 'A';  
+        }
+        number = number / base;
+        i++;
     }
-    result[i] = '\0';
-    reverseString(result); // Reverse the result string
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%c", result[j]);
+    }
+    printf("\n");
+    return 0;
 }
 
-// Function to reverse a string
-void reverseString(char *str) {
-    int length = strlen(str);
-    int i, j;
-    for (i = 0, j = length - 1; i < j; i++, j--) {
-        char temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
+// Task 2: Converting a given base number to decimal number.
+int task2(char *number, int base) {
+    int result = 0;
+    int length = strlen(number);
+    int power = 0;
+    for (int i = length - 1; i >= 0; i--) {
+        char digit = number[i];
+        int value;
+        if (isdigit(digit)) {
+            value = digit - '0';
+        } else if (isxdigit(digit)) {
+            value = toupper(digit) - 'A' + 10;
+        } else {
+            printf("Check your input again!\n");
+            return 1;
+        }
+        result += value * pow(base, power);
+        power++;
     }
+    printf("%d\n", result);
+    return 0;
 }
