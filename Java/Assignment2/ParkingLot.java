c76c95c863dda56719ebb1815a90afa593620d7c
Amuujin Erdenesaikhan <amuujine@gmail.com>
2023-11-06 13:08:35 +0900

    some features on java code

--- Java/Assignment2/ParkingLot.java
+++ Java/Assignment2/ParkingLot.java
@@ -1,176 +1,124 @@
 package Assignment2;
-//ParkingLot.java
+
 import java.util.Scanner;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.io.FileNotFoundException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class ParkingLot {
-    private static ArrayList<Resident> residents = new ArrayList<>();
+    // Declaration
     private static ParkingSpot[] parkingSpots;
     private static int feeMonthly = 0;
     private static int feePerTen = 0;
+    private static int totalIncome = 0;
+    private static int numSpots = 0;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Starting ...");
         System.out.println("Enter the number of parking spots:");
-        int numSpots = in.nextInt();
+        numSpots = in.nextInt();
         System.out.println("Enter the monthly parking fee for residents:");
         feeMonthly = in.nextInt();
         System.out.println("Enter the parking fee per 10 minutes:");
         feePerTen = in.nextInt();
-        
+
         // Initialize the parking spots array
         parkingSpots = new ParkingSpot[numSpots];
-        for(int i = 0; i < numSpots; i++){
+        for (int i = 0; i < numSpots; i++) {
             parkingSpots[i] = new ParkingSpot();
         }
 
-        //Read the resident data
-        try {
-            readResidentData();
-        } catch (FileNotFoundException e){
-            System.out.println("res.txt file not found.");
-            return;
+        // check res.txt file existing
+        File f = new File("src/Assignment2/res.txt");
+        if (f.exists() && !f.isDirectory()) {
+            System.out.println("The file res.txt exists.");
+        } else {
+            System.out.println("The file res.txt does not exist.");
         }
+        System.out.println();
+
+        // Procedure part
         while (true) {
-            System.out.println("Enter a command:");
+            System.out.println("Parking System is working. Enter the command: ");
             String command = in.next();
             switch (command.toLowerCase()) {
                 case "a":
-                    // Call method to assign a parking spot
-                    assignParkingSpot(in);
+                    String input = in.nextLine().trim();
+                    assignPS(input);
                     break;
                 case "w":
-                    // Call method to withdraw a parking assignment
-                    withdrawParking(in);
+                    // withdrawPS();
                     break;
                 case "e":
-                    // Call method to extend parking time
-                    extendParking(in);
+                    // extend();
                     break;
                 case "x":
-                    // Call method to calculate parking fee
-                    calculateFee(in);
+                    // calculate();
                     break;
                 case "s":
-                    // Call method to show parking spots status
-                    showParkingStatus();
+                    // statusPS();
                     break;
                 case "i":
-                    // Call method to show income
-                    showIncome(in);
+                    // income();
                     break;
-                case "q":
-                    // Command to quit the program
-                    System.out.println("Exiting program.");
-                    in.close();
-                    return;
                 default:
-                    System.out.println("Invalid command.");
+                    System.out.println("Invalid input!");
             }
+            System.out.println();
         }
     }
-    private static void readResidentData() throws FileNotFoundException {
-        Scanner fileScanner = new Scanner(new File("res.txt"));
-        while (fileScanner.hasNextLine()) {
-            String line = fileScanner.nextLine();
-            String[] data = line.split(" ");
-            if (data.length == 2) {
-                residents.add(new Resident(data[1], data[0]));
-            }
-        }
-        fileScanner.close();
-    }
-    // Placeholder methods for each command:
-    private static void assignParkingSpot(Scanner in) {
-        // Implement this method to handle parking spot assignment
-    }
 
-    private static void withdrawParking(Scanner in) {
-        // Implement this method to handle parking withdrawal
-    }
-
-    private static void extendParking(Scanner in) {
-        // Implement this method to handle parking extension
-    }
-
-    private static void calculateFee(Scanner in) {
-        // Implement this method to handle fee calculation
-    }
-
-    private static void showParkingStatus() {
-        // Implement this method to show parking spots status
-    }
-
-    private static void showIncome(Scanner in) {
-        // Implement this method to show income from parking fees
-    }
-    // Inside ParkingLot.java
-
-    // This method needs to be implemented to find a free parking spot
-    private static ParkingSpot findFreeParkingSpot() {
+    // Method to count available parking spots
+    private static int getAvailableSpotsCount() {
+        int count = 0;
         for (ParkingSpot spot : parkingSpots) {
-            if (!spot.isOccupied()) {
-                return spot;
+            if (!spot.isAvailable()) {
+                count++;
             }
         }
-        return null; // If no spots are free
+        return count;
     }
 
-    // This method needs to be implemented to convert input data to Date
-    private static LocalDateTime convertToDate(int year, int month, int day) {
-        return LocalDateTime.of(year, month, day, 0, 0);
-    }
-
-    // This method needs to be implemented to find a resident by phone number
-    private static Resident findResidentByPhoneNumber(String phoneNumber) {
-        for (Resident resident : residents) {
-            if (resident.getPhoneNumber().equals(phoneNumber)) {
-                return resident;
-            }
+    // Assign Parking Spot method
+    private static void assignPS(String input) {
+        // Split the input string by spaces to get the individual parts
+        String[] parts = input.split(" ");
+        // Assuming the input format is: a carNumber phoneNumber year month day
+        String carNumber = parts[0];
+        System.out.println("CarNumber: "+carNumber);
+        String phoneNumber = parts[1];
+        System.out.println("Phone: "+phoneNumber);
+        // Validate the carNumber to be a 4-digit number
+        if (!carNumber.matches("\\d{4}")) {
+            System.out.println("Error: Car number must be in 4 digits.");
+            return;
         }
-        return null; // If resident is not found
-    }
-
-    // Implementation of assignParkingSpot
-    private static void assignParkingSpot(in) {
-        String carPlate = in.next();
-        String phoneNumber = in.next();
-        int year = in.nextInt();
-        int month = in.nextInt();
-        int day = in.nextInt();
-
-        Resident resident = findResidentByPhoneNumber(phoneNumber);
-        ParkingSpot spot = findFreeParkingSpot();
-
-        if (spot != null && resident != null) {
-            spot.occupySpot(carPlate, true, convertToDate(year, month, day).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
-            System.out.printf("%s차량에 주차공간(#%d 번)이 배정되었습니다!%n", carPlate, Arrays.asList(parkingSpots).indexOf(spot) + 1);
-        } else {
-            System.out.println("주차 공간이 없거나 거주자 정보를 찾을 수 없습니다.");
-        }
-    }
-
-    // Implementation of withdrawParking
-    private static void withdrawParking(in) {
-        String carPlate = in.next();
-        int year = in.nextInt();
-        int month = in.nextInt();
-        int day = in.nextInt();
-
-        // Search for the car in the parking spots
-        for (int i = 0; i < parkingSpots.length; i++) {
-            if (parkingSpots[i].isOccupied() && parkingSpots[i].getCarPlate().equals(carPlate)) {
-                // Found the car, now withdraw
-                System.out.printf("거주자 우선주차 차량 %s 가 주차공간(#%d) 배정을 철회하였습니다!%n", carPlate, i + 1);
-                parkingSpots[i].freeSpot();
+        int year = Integer.parseInt(parts[2]);
+        System.out.println("year: "+year);
+        int month = Integer.parseInt(parts[3]);
+        System.out.println("month: "+month);
+        int day = Integer.parseInt(parts[4]);
+        System.out.println("day: "+ day);
+        // Construct the assignment date in the proper format
+        LocalDate assignmentDate = LocalDate.of(year, month, day);
+        String formattedDate = assignmentDate.format(DateTimeFormatter.ofPattern("yyyy MM dd"));
+        boolean spotAssigned = false;
+        for (int i = 0; i < parkingSpots.length && !spotAssigned; i++) {
+            // Check if the carNumber is already assigned
+            if (parkingSpots[i].getResidentCarNumber() != null && parkingSpots[i].getResidentCarNumber().equals(carNumber)) {
+                System.out.println("Error: This car number is already assigned to a parking spot.");
                 return;
             }
+            if (parkingSpots[i].isAvailable()) {
+                parkingSpots[i].assignToResident(carNumber, phoneNumber, formattedDate);
+                System.out.println(carNumber + "차량에 주차공간(#" + (i + 1) + " 번)이 배정되었습니다!");
+                System.out.println("Remaining parking spots: " + getAvailableSpotsCount());
+                spotAssigned = true;
+            }
         }
-        System.out.println("해당 차량은 주차되어 있지 않습니다.");
+        if (!spotAssigned) {
+            System.out.println("No available parking spots.");
+        }
     }
 }
--- Java/Assignment2/ParkingSpot.java
+++ Java/Assignment2/ParkingSpot.java
@@ -1,135 +1,33 @@
 package Assignment2;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-//ParkingSpot.java
-class ParkingSpot {
+
+public class ParkingSpot {
     private boolean isOccupied;
-    private String carPlate; // or another identifier for a car
-    private boolean isResident;
-    private long arrivalTime; // Store time of arrival for calculating parking time
-
-    // Constructors, getters, and setters
-
-    public boolean isOccupied() {
-        return isOccupied;
-    }
-
-    public void occupySpot(String carPlate, boolean isResident, long arrivalTime) {
-        this.isOccupied = true;
-        this.carPlate = carPlate;
-        this.isResident = isResident;
-        this.arrivalTime = arrivalTime;
-    }
-
-    public void freeSpot() {
+    private String residentCarNumber;
+    private String residentPhoneNumber;
+    private LocalDate assignmentDate;
+    public ParkingSpot() {
+        // When a parking spot is created, it's initially not occupied
         this.isOccupied = false;
-        this.carPlate = null;
-        this.arrivalTime = 0;
-        this.isResident = false;
     }
-
-    public String getCarPlate() {
-        return carPlate;
+    public boolean isAvailable() {
+        return !isOccupied;
     }
-
-    public boolean isResident() {
-        return isResident;
+    public String getResidentCarNumber() {
+        return residentCarNumber;
     }
-
-    public long getArrivalTime() {
-        return arrivalTime;
-    }
-
-    public long getParkingDuration() {
-        return System.currentTimeMillis() - arrivalTime;
-    }
-
-    // Consider adding a method to calculate the fee, depending on the duration and whether the car belongs to a resident.
-    public double calculateFee(long feePerTen, long feeMonthly) {
-        if (this.isResident) {
-            return feeMonthly;
-        } else {
-            long durationInMinutes = getParkingDuration() / (1000 * 60);
-            return (double) ((durationInMinutes / 10) + 1) * feePerTen;
+    public void assignToResident(String carNumber, String phoneNumber, String formattedDate) {
+        this.residentCarNumber = carNumber;
+        this.residentPhoneNumber = phoneNumber;
+        try {
+            this.assignmentDate = LocalDate.parse(formattedDate.trim(), DateTimeFormatter.ofPattern("yyyy MM dd"));
+            this.isOccupied = true;
+        } catch (DateTimeParseException e) {
+            System.out.println("Error: Invalid date format provided.");
         }
     }
-
-    // This method needs to be implemented to find a free parking spot
-    private static ParkingSpot findFreeParkingSpot() {
-        for (ParkingSpot spot : parkingSpots) {
-            if (!spot.isOccupied()) {
-                return spot;
-            }
-        }
-        return null; // If no spots are free
-    }
-
-    // This method needs to be implemented to convert input data to Date
-    private static LocalDateTime convertToDate(int year, int month, int day) {
-        return LocalDateTime.of(year, month, day, 0, 0);
-    }
-
-    // This method needs to be implemented to find a resident by phone number
-    private static Resident findResidentByPhoneNumber(String phoneNumber) {
-        for (Resident resident : residents) {
-            if (resident.getPhoneNumber().equals(phoneNumber)) {
-                return resident;
-            }
-        }
-        return null; // If resident is not found
-    }
-
-    // Implementation of assignParkingSpot
-    private static void assignParkingSpot(Scanner in) {
-        String carPlate = in.next();
-        String phoneNumber = in.next();
-        int year = in.nextInt();
-        int month = in.nextInt();
-        int day = in.nextInt();
-
-        Resident resident = findResidentByPhoneNumber(phoneNumber);
-        ParkingSpot spot = findFreeParkingSpot();
-
-        if (spot != null && resident != null) {
-            spot.occupySpot(carPlate, true, convertToDate(year, month, day).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
-            System.out.printf("%s차량에 주차공간(#%d 번)이 배정되었습니다!%n", carPlate, Arrays.asList(parkingSpots).indexOf(spot) + 1);
-        } else {
-            System.out.println("주차 공간이 없거나 거주자 정보를 찾을 수 없습니다.");
-        }
-    }
-
-    // Implementation of withdrawParking
-    private static void withdrawParking(Scanner in) {
-        String carPlate = in.next();
-        int year = in.nextInt();
-        int month = in.nextInt();
-        int day = in.nextInt();
-
-        // Search for the car in the parking spots
-        for (int i = 0; i < parkingSpots.length; i++) {
-            if (parkingSpots[i].isOccupied() && parkingSpots[i].getCarPlate().equals(carPlate)) {
-                // Found the car, now withdraw
-                System.out.printf("거주자 우선주차 차량 %s 가 주차공간(#%d) 배정을 철회하였습니다!%n", carPlate, i + 1);
-                parkingSpots[i].freeSpot();
-                return;
-            }
-        }
-        System.out.println("해당 차량은 주차되어 있지 않습니다.");
-    }
-
-    @Override
-    public String toString() {
-        return "ParkingSpot{" +
-                "isOccupied=" + isOccupied +
-                ", carPlate='" + carPlate + '\'' +
-                ", isResident=" + isResident +
-                ", arrivalTime=" + arrivalTime +
-                '}';
-    }
-
-    // Add any other necessary methods
 }
