5d037f0db33f858605bf4c678b34ebec7fc86848
Amuujin Erdenesaikhan <amuujine@gmail.com>
2023-11-06 23:01:05 +0900

    Some code features of java codes

--- Java/Assignment2/ParkingLot.java
+++ Java/Assignment2/ParkingLot.java
@@ -1,8 +1,15 @@
 package Assignment2;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class ParkingLot {
@@ -10,7 +17,6 @@ public class ParkingLot {
     private static ParkingSpot[] parkingSpots;
     private static int feeMonthly = 0;
     private static int feePerTen = 0;
-    private static int totalIncome = 0;
     private static int numSpots = 0;
 
     public static void main(String[] args) {
@@ -29,15 +35,6 @@ public class ParkingLot {
             parkingSpots[i] = new ParkingSpot();
         }
 
-        // check res.txt file existing
-        File f = new File("src/Assignment2/res.txt");
-        if (f.exists() && !f.isDirectory()) {
-            System.out.println("The file res.txt exists.");
-        } else {
-            System.out.println("The file res.txt does not exist.");
-        }
-        System.out.println();
-
         // Procedure part
         while (true) {
             System.out.println("Parking System is working. Enter the command: ");
@@ -52,17 +49,19 @@ public class ParkingLot {
                     wipeOutResidents(inputW);
                     break;
                 case "e":
-                    // String enterInput = in.nextLine().trim();
-                    // enter(enterInput);
+                    String enterInput = in.nextLine().trim();
+                    enter(enterInput);
                     break;
                 case "x":
-                    // calculate();
+                    String leaveInput = in.nextLine().trim();
+                    leave(leaveInput);
                     break;
                 case "s":
-                    // statusPS();
+                    statusPS();
                     break;
                 case "i":
-                    // income();
+                    String incomeInput = in.nextLine().trim();
+                    income(incomeInput);
                     break;
                 default:
                     System.out.println("Invalid input!");
@@ -113,6 +112,7 @@ public class ParkingLot {
         if (!spotAssigned) {
             System.out.println("No available parking spots.");
         }
+    }
 
     // Method to wipe out resident parking assignments
     private static void wipeOutResidents(String input) {
@@ -143,10 +143,205 @@ public class ParkingLot {
                 }
             }
         }
-
         // If the car number was not found or the date did not match
         if (!foundCar) {
             System.out.println("Error: Car number " + carNumber + " with given date not found.");
         }
+    }
+
+    // Method to check if a car number belongs to a resident
+    private static boolean isResidentCarNumber(String carNumber) {
+        try {
+            File file = new File("src/Assignment2/res.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                // If the car number is the second word in the line.
+                String[] lineParts = line.split(" ");
+                if (lineParts.length > 1 && lineParts[1].equals(carNumber)) {
+                    scanner.close();
+                    return true;
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: The resident file was not found.");
+        } catch (Exception e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+        return false;
+    }
+
+    private static void enter(String enterInput) {
+        String[] parts = enterInput.split(" ");
+        // Assuming the first part is 'e' for enter command, followed by car number and
+        // optional phone number.
+        String carNumber = parts[1];
+        String phoneNumber = parts.length == 7 ? null : parts[2]; // phone number is present only for non-residents.
+
+        // Parse the date and time from the input.
+        int year = Integer.parseInt(parts[parts.length - 5]);
+        int month = Integer.parseInt(parts[parts.length - 4]);
+        int day = Integer.parseInt(parts[parts.length - 3]);
+        int hour = Integer.parseInt(parts[parts.length - 2]);
+        int minute = Integer.parseInt(parts[parts.length - 1]);
+
+        // Create date and time objects.
+        LocalDate date = LocalDate.of(year, month, day);
+        LocalTime time = LocalTime.of(hour, minute);
+
+        // Determine if it's a resident or non-resident based on the presence of a phone
+        // number.
+        boolean isResident = phoneNumber == null;
+
+        // Iterate over parking spots to find an available one or a reserved spot for a
+        // resident vehicle.
+        for (int i = 0; i < parkingSpots.length; i++) {
+            ParkingSpot spot = parkingSpots[i];
+
+            if (isResident && spot.isReservedFor(carNumber)) {
+                // For resident, if the spot is reserved for this car, mark it as occupied.
+                spot.markAsOccupied();
+                System.out.println("거주자 우선 주차차량 " + carNumber + "가 입차하였습니다!");
+                return;
+            } else if (!isResident && spot.isAvailable()) {
+                // For non-resident, assign the first available spot.
+                spot.assignToNonResident(carNumber, phoneNumber, date, time);
+                spot.markAsOccupied(); // Mark the spot as occupied.
+                System.out.println("일반 차량 " + carNumber + "이 입차하였습니다! 주차공간 " + (i + 1) + "번이 배정되었습니다!");
+                return;
+            }
+        }
+
+        System.out.println("Error: No available parking spots.");
+    }
+
+    // Method for a car leaving the parking lot
+    private static void leave(String leaveInput) {
+        // Parse the input string
+        String[] parts = leaveInput.split(" ");
+        String carNumber = parts[0];
+        int year = Integer.parseInt(parts[1]);
+        int month = Integer.parseInt(parts[2]);
+        int day = Integer.parseInt(parts[3]);
+        int hour = Integer.parseInt(parts[4]);
+        int minute = Integer.parseInt(parts[5]);
+
+        // Construct the leaving date and time
+        LocalDate leaveDate = LocalDate.of(year, month, day);
+        LocalTime leaveTime = LocalTime.of(hour, minute);
+
+        // Loop through the parking spots to find the car
+        for (ParkingSpot spot : parkingSpots) {
+            // Check if the current spot has the car and is occupied
+            if (spot.getAssignedCarNumber() != null && spot.getAssignedCarNumber().equals(carNumber)
+                    && spot.isOccupied()) {
+                // Calculate parking duration manually in minutes
+                long minutesParked = Duration.between(
+                        LocalDateTime.of(spot.getAssignmentDate(), spot.getAssignmentTime()),
+                        LocalDateTime.of(leaveDate, leaveTime)).toMinutes();
+
+                // If the car is not a resident, calculate the fee
+                int fee = 0;
+                if (!spot.isResidentOccupying()) { // Changed method call to isResidentOccupying
+                    fee = (int) ((minutesParked / 10) * feePerTen);
+                }
+
+                // Output the car number, parking time and fee
+                System.out.println("일반 차량 " + carNumber + "이 출차하였습니다!");
+                System.out.println("주차시간: " + minutesParked + "분");
+                System.out.println("주차요금: " + fee + "원");
+
+                // Free the parking spot
+                spot.clearAssignment();
+                break;
+            }
+        }
+    }
+
+    private static void statusPS() {
+        ArrayList<String> residentInfo = new ArrayList<>();
+        ArrayList<String> nonResidentInfo = new ArrayList<>();
+
+        // Collect data for all parked cars
+        for (ParkingSpot spot : parkingSpots) {
+            if (spot.isOccupied()) {
+                String info = spot.getParkingSpotNumber() + " " + spot.getAssignedCarNumber() + " "
+                        + spot.getContactNumber();
+                if (spot.isResidentOccupying()) {
+                    // Assuming you have a method to get resident's name and the assignment date
+                    info += " " + spot.getResidentName() + " " + spot.getAssignmentDateFormatted();
+                    residentInfo.add(info);
+                } else {
+                    // For non-residents, include the time they entered
+                    info += " " + spot.getAssignmentDateFormatted() + " " + spot.getAssignmentTimeFormatted();
+                    nonResidentInfo.add(info);
+                }
+            }
+        }
+
+        // Sort the lists based on the car number
+        Comparator<String> byCarNumber = Comparator.comparing(s -> s.split(" ")[1]);
+        residentInfo.sort(byCarNumber);
+        nonResidentInfo.sort(byCarNumber);
+
+        // Print out the resident information
+        System.out.println("거주자 우선주차 차량:");
+        for (String info : residentInfo) {
+            System.out.println(info);
+        }
+
+        // Print out the non-resident information
+        System.out.println("일반 차량:");
+        for (String info : nonResidentInfo) {
+            System.out.println(info);
+        }
+    }
+
+    // Method to calculate income for a specific month and year
+    private static void income(String incomeInput) {
+        String[] parts = incomeInput.split(" ");
+        int year = Integer.parseInt(parts[1]);
+        int month = Integer.parseInt(parts[2]);
+
+        // Variables to hold income from residents and non-residents
+        int incomeFromResidents = 0;
+        int incomeFromNonResidents = 0;
+
+        // Lists to keep track of resident car income details
+        List<String> residentCarDetails = new ArrayList<>();
+
+        // Iterate over all parking spots to calculate income
+        for (ParkingSpot spot : parkingSpots) {
+            // Check if the spot has an assigned car and if the assignment date matches the
+            // input month and year
+            if (spot.getAssignedCarNumber() != null && spot.getAssignmentDate() != null) {
+                LocalDate assignmentDate = spot.getAssignmentDate();
+                if (assignmentDate.getYear() == year && assignmentDate.getMonthValue() == month) {
+                    if (spot.isResidentOccupying()) {
+                        // Add monthly fee for residents
+                        incomeFromResidents += feeMonthly;
+                        residentCarDetails.add(spot.getAssignedCarNumber() + ": " + feeMonthly + "원");
+                    } else {
+                        // Calculate the income from non-residents based on parked duration
+                        long minutesParked = Duration.between(
+                                LocalDateTime.of(assignmentDate, spot.getAssignmentTime()),
+                                LocalDateTime.now() // Assuming current time for the sake of this example
+                        ).toMinutes();
+                        int fee = (int) ((minutesParked / 10) * feePerTen);
+                        incomeFromNonResidents += fee;
+                    }
+                }
+            }
+        }
+
+        // Output the total income
+        System.out
+                .println("총수입 " + year + "년 " + month + "월 원: " + (incomeFromResidents + incomeFromNonResidents) + "원");
+        System.out.println("거주자 우선주차 차량: " + incomeFromResidents + " 원");
+        for (String detail : residentCarDetails) {
+            System.out.println(detail);
+        }
+        System.out.println("일반 차량: " + incomeFromNonResidents + " 원");
     }
 }
--- Java/Assignment2/ParkingSpot.java
+++ Java/Assignment2/ParkingSpot.java
@@ -1,19 +1,31 @@
 package Assignment2;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class ParkingSpot {
     private boolean isOccupied;
     private String carNumber;
-    private String phoneNumber;
     private LocalDate assignmentDate;
+    private LocalTime assignmentTime;
     private boolean isResident; // This field indicates if the spot is taken by a resident
+    private String phoneNumber;
+    private int parkingSpotNumber;
+    private String residentName;
 
     public ParkingSpot() {
         this.isOccupied = false;
         this.isResident = false;
+    }
+
+    public void markAsOccupied() {
+        this.isOccupied = true;
+    }
+
+    public boolean isReservedFor(String carNumber) {
+        return this.isResident && this.carNumber.equals(carNumber);
     }
 
     public String getAssignedCarNumber() {
@@ -28,6 +40,10 @@ public class ParkingSpot {
         return carNumber;
     }
 
+    public void setOccupied(boolean occupied) {
+        this.isOccupied = occupied;
+    }
+
     public LocalDate getAssignmentDate() {
         return assignmentDate;
     }
@@ -40,6 +56,52 @@ public class ParkingSpot {
         return isOccupied;
     }
 
+    public int getParkingSpotNumber() {
+        return this.parkingSpotNumber;
+    }
+
+    public String getContactNumber() {
+        return this.phoneNumber;
+    }
+
+    public String getResidentName() {
+        // This method would need to retrieve the resident's name.
+        // How this is done depends on where the resident information is stored.
+        return this.residentName;
+    }
+
+    public String getAssignmentDateFormatted() {
+        if (this.assignmentDate != null) {
+            return this.assignmentDate.format(DateTimeFormatter.ofPattern("yyyy MM dd"));
+        }
+        return "";
+    }
+
+    public String getAssignmentTimeFormatted() {
+        if (this.assignmentTime != null) {
+            return this.assignmentTime.format(DateTimeFormatter.ofPattern("HH mm"));
+        }
+        return "";
+    }
+
+    // For assigning to residents
+    public void assignToResident(String carNumber) {
+        this.carNumber = carNumber;
+        this.isOccupied = true;
+        this.isResident = true;
+    }
+
+    // For assigning to non-residents
+    public void assignToNonResident(String carNumber, String phoneNumber, LocalDate assignmentDate,
+            LocalTime assignmentTime) {
+        this.carNumber = carNumber;
+        this.phoneNumber = phoneNumber;
+        this.assignmentDate = assignmentDate;
+        this.assignmentTime = assignmentTime;
+        this.isOccupied = true;
+        this.isResident = false;
+    }
+
     public boolean isResidentOccupying() {
         return isOccupied && isResident;
     }
@@ -50,6 +112,14 @@ public class ParkingSpot {
 
     public void assignToNonResident(String carNumber, String phoneNumber, String formattedDate) {
         assign(carNumber, phoneNumber, formattedDate, false);
+    }
+
+    public LocalTime getAssignmentTime() {
+        return assignmentTime;
+    }
+
+    public void setAssignmentTime(LocalTime assignmentTime) {
+        this.assignmentTime = assignmentTime;
     }
 
     // This method clears the assignment
